9a2cb6d0ad700212d792c282274f7ede8f69fb00
Tom <tomfokker2000@hotmail.com>
2018-09-15 12:25:53 +0200

    Endpoints voor bedrijven is aangemaakt, er moeten nog endpoints voor reviews van bedrijven bij komen

---
+++ PoohAPI/Models/Company.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PoohAPI.Models
+{
+    // Class with all information of the company
+    public class Company : BaseCompany
+    {
+        public string Description { get; set; }
+        public string EmailAddress { get; set; }
+        public string Website { get; set; }
+    }
+}
