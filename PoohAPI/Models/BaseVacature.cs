51bf916409fd9328cd88ab54a9ab8bf8bf3e8253
Tom <tomfokker2000@hotmail.com>
2018-09-15 10:27:31 +0200

    project is weer hersteld

---
+++ PoohAPI/Models/BaseVacature.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PoohAPI.Models
+{
+    //Class that contains basic information about a vacature.
+    public class BaseVacature
+    {
+        [Required]
+        public int Id { get; set; } 
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public DateTime CreationDate { get; set; }
+        [Required]
+        public IEnumerable<string> Education { get; set; }
+        public string Location { get; set; }
+        [Required]
+        public string Description { get; set; }
+        public string Company { get; set; }
+    }
+}
