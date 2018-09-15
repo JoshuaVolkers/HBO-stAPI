9fb13fab733b5c561fc17149f0ff14253aaaef9a
BigHos69 <joshuadaveyvolkers@hotmail.com>
2018-09-15 15:41:27 +0200

    Vacancies can now be filtered
    Vacancies can now be filtered based on the specs the Mobile group sent our way

---
+++ PoohAPI/IntershipType.cs
@@ -0,0 +1,14 @@
+﻿namespace PoohAPI
+{
+    public enum IntershipType
+    {
+        /// <summary>
+        /// Meeloopstage/snuffelstage/internship, normally the type of internship students follow to get an introduction into the workfield.
+        /// </summary>
+        Meeloopstage = 1,
+        /// <summary>
+        /// Afstudeerstage/graduation internship, the type of intership that students perform in order graduate for their degree.
+        /// </summary>
+        Afstudeerstage
+    }
+}
