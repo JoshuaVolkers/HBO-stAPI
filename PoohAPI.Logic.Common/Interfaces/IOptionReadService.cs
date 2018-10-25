b7fbda876eac43f0c54600e3b1e37b8aa83883b7
fabianknop <fabianknop@ziggo.nl>
2018-10-25 13:42:32 +0200

    .

---
+++ PoohAPI.Logic.Common/Interfaces/IOptionReadService.cs
@@ -0,0 +1,14 @@
+﻿using PoohAPI.Models.OptionModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoohAPI.Logic.Common.Interfaces
+{
+    interface IOptionReadService
+    {
+        IEnumerable<Major> GetAllMajors(int maxCount, int offset);
+    }
+}
---
+++ PoohAPI.Logic.Options/Services/OptionsReadService.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using PoohAPI.Infrastructure.OptionDB.Repositories;
+using PoohAPI.Logic.Common.Interfaces;
+using PoohAPI.Logic.Common.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PoohAPI.Logic.Options.Services
+{
+    public class OptionReadService : IOptionReadService
+    {
+        private readonly IOptionRepository optionRepository;
+        private readonly IMapper _mapper;
+
+        public OptionReadService(IOptionRepository optionRepository, IMapper mapper)
+        {
+            _optionRepository = optionRepository;
+            _mapper = mapper;
+        }        
+    }
+}
