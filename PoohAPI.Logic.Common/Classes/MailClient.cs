1e6105119c662107f916839e3778157d9cbef9d2
Umeeeeer <umeer.arshad@hotmail.com>
2018-10-26 02:37:16 +0200

    Revert "Merge branch 'dev' into Umeer2"
    This reverts commit 66815bfecdce8fa8b2515ac51b2be9556d1a2ff9, reversing
    changes made to 0b371d1c395aad2656773096fa89aad0d7e936dd.

--- PoohAPI.Logic.Common/Classes/MailClient.cs
+++ PoohAPI.Logic.Common/Classes/MailClient.cs
@@ -5,36 +5,28 @@ using System.Linq;
 using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
-using Microsoft.Extensions.Configuration;
 
 namespace PoohAPI.Logic.Common.Classes
 {
     public class MailClient : IMailClient
     {
-        private readonly IConfiguration config;
-
-        public MailClient(IConfiguration config)
-        {
-            this.config = config;
-        }
-
         public void SendEmail(string userEmail, string subject, string body)
         {
             if (userEmail is null || subject is null || body is null)
                 return;
 
             // TODO: change sender email address and host
-            MailMessage mail = new MailMessage(this.config.GetValue<string>("MailSender"), userEmail);
+            MailMessage mail = new MailMessage("sender@test.nl", userEmail);
             mail.IsBodyHtml = true;
             mail.BodyEncoding = Encoding.UTF8;
             mail.Subject = subject;
             mail.Body = body;
 
             SmtpClient client = new SmtpClient();
-            client.Port = this.config.GetValue<int>("MailPort");
+            client.Port = 25;
             client.DeliveryMethod = SmtpDeliveryMethod.Network;
             client.UseDefaultCredentials = false;
-            client.Host = this.config.GetValue<string>("MailHost");
+            client.Host = "localhost";
             
             client.Send(mail);
         }
