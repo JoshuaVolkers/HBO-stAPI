562a6d56959d9962bb558132f265079c9b5f7ac7
Joshua <joshuadaveyvolkers@hotmail.com>
2018-11-05 17:55:13 +0100

    Update Scripts/Tests/test_api_post_review.py

--- Scripts/Tests/test_api_post_review.py
+++ Scripts/Tests/test_api_post_review.py
@@ -6,10 +6,12 @@ import datetime
 from cerberus import Validator
 import json
 
+
 def f_test_review(review):
     v = Validator(api_schemas.review_schema)
     correct = v.validate(review)
     assert correct == True
+
 
 def f_test_review_input(review, companyId, stars, writtenReview, anonymous):
     correct = True
@@ -27,21 +29,56 @@ def f_test_review_input(review, companyId, stars, writtenReview, anonymous):
 
     assert correct == True
 
+def f_test_review_update_input(review, id, companyId, stars, writtenReview, anonymous, creationDate, verifiedReview, verifiedBy):
+    correct = True
+    if review["id"] != id:
+        correct = False
+
+    if review["companyId"] != companyId:
+        correct = False
+
+    if review["stars"] != stars:
+        correct = False
+
+    if review["writtenReview"] != writtenReview:
+        correct = False
+
+    if review["anonymous"] != anonymous:
+        correct = False
+
+    if review["creationDate"] != creationDate:
+        correct = False
+
+    if review["verifiedReview"] != verifiedReview:
+        correct = False
+
+    if review["verifiedBy"] != verifiedBy:
+        correct = False
+
+    assert correct == True
+
+
 def f_test_status_code_200(request):
     assert request.status_code == 200
+
 
 base_url = ''
 reviews_path = "reviews"
 users_path = "users"
 token = {}
 headers = ''
+createdReviewId = ''
 
-#set global base_url variable
+# set global base_url variable
+
+
 def test_api_path(apiurl):
     global base_url
     base_url = apiurl
 
 # login with student credentials
+
+
 @pytest.mark.unit
 def test_login(studentemail, studentpass):
     payload = {'emailAddress': studentemail, 'password': studentpass}
@@ -50,9 +87,11 @@ def test_login(studentemail, studentpass):
     token = login_request.json()
 
     global headers
-    headers = {'Authorization' : 'Bearer '+token}
+    headers = {'Authorization': 'Bearer '+token}
 
-# Test review update
+# Test review post
+
+
 def test_post_new_review():
     companyId = 1
     stars = 3
@@ -64,9 +103,54 @@ def test_post_new_review():
         'stars': stars,
         'writtenReview': writtenReview,
         'anonymous': anonymous,
-        }
-    review_request = requests.post(base_url+reviews_path, json=payload, headers=headers)
+    }
+
+    review_request = requests.post(
+        base_url+reviews_path, json=payload, headers=headers)
     f_test_status_code_200(review_request)
     review = review_request.json()
     f_test_review(review)
     f_test_review_input(review, companyId, stars, writtenReview, anonymous)
+
+    global createdReviewId
+    createdReviewId = review['Id']
+
+# Test review update
+
+
+def test_update_created_review():
+    reviewId = createdReviewId
+    companyId = 1
+    stars = 4
+    writtenReview = "TEST_ update, nog steeds prima"
+    anonymous = 0
+    creationDate = datetime.datetime.now()
+    verifiedReview = 0
+    verifiedBy = 0
+
+    payload = {
+        'id': reviewId,
+        'companyId': companyId,
+        'stars': stars,
+        'writtenreview': writtenReview,
+        'anonymoys': anonymous,
+        'creationDate': creationDate,
+        'verifiedReview': verifiedReview,
+        'verifiedBy': verifiedBy
+    }
+
+    review_update_request = requests.put(base_url+reviews_path+"/"+str(reviewId), json=payload, headers=headers)
+    f_test_status_code_200(review_update_request)
+    review = review_update_request.json()
+    f_test_review(review)
+    f_test_review_update_input(review, reviewId, companyId, stars, writtenReview, anonymous, creationDate, verifiedReview, verifiedBy)
+
+def test_get_updated_review():
+    review_get_request = requests.get(base_url+reviews_path+"/"+str(createdReviewId), headers=headers)
+    f_test_status_code_200(review_get_request)
+    review = review_get_request.json()
+    f_test_review(review)
+
+def test_delete_review():
+    review_delete_request = requests.delete(base_url+reviews_path+"/"+str(createdReviewId), headers=headers)
+    f_test_status_code_200(review_delete_request)
